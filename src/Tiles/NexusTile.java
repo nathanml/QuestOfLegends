4def953333c8f487160259b604491cc4fcd0dfaa
nathanml <nathanml@bu.edu>
2020-04-10 13:09:25 -0400

    Modify pieces and board

---
+++ src/Tiles/NexusTile.java
@@ -0,0 +1,16 @@
+package Tiles;
+
+import Main.Piece;
+
+public class NexusTile extends Tile {
+    public NexusTile() {
+        super ();
+        name = "Nexus Tile";
+        val = 'N';
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
